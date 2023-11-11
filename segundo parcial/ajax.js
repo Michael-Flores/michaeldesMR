4c826d110e9d8a22042ab294bfe20b65c0910ec2
Michael-Flores <micha11.mrfv@gmail.com>
2023-11-11 10:30:01 -0400

    624587

--- segundo parcial/ajax.js
+++ segundo parcial/ajax.js
@@ -34,36 +34,36 @@ function cambiartitulo(pre1) {
     var titulo1=document.getElementById('titulo');
     titulo1.innerHTML=pre1;
 }
-function cargar3(){
+
+
+function cargar3() {
     var contenedor;
     contenedor = document.getElementById('cuadros');
     var formulario = document.getElementById("form-1");
     var parametros = new FormData(formulario);
-    var ajax = new XMLHttpRequest();
-    ajax.open('post','introducir.php',true);
-    ajax.onreadystatechange = function(){
-        if(ajax.readyState == 4){
-            contenedor.innerHTML = ajax.responseText;
-        }
-    }    
 
-ajax.send(parametros);
-}
-
-function update()
-{
-    var contenedor;
-    contenedor = document.getElementById('cuadros');
-    var formulario = document.getElementById("form-alumno");
-    var parametros = new FormData(formulario);
-
+    var cam=document.getElementById('principal');
+    f=new FormData();
+    f.add
     var ajax = new XMLHttpRequest() //crea el objetov ajax 
     ajax.open("post", 'introducir.php' , true);
-    ajax.onreadystatechange = function ( ) {
+    ajax.onreadystatechange = function () {
         if (ajax.readyState == 4) {
-            contenedor.innerHTML = ajax.responseText; 
+            contenedor.innerHTML = ajax.responseText;
+            cam.style.height="auto";
+            let cuadros = document.querySelectorAll('#cuadros input');
+            let suma = 0;
+            cuadros.forEach(cuadro => {
+              cuadro.addEventListener('input', () => {
+                suma = 0;
+                cuadros.forEach(c => {
+                  suma += parseInt(c.value) || 0;
+                });
+                document.getElementById('suma').textContent = suma;
+              });
+            });
         }
     }
 
-    ajax.send(parametros );    
+    ajax.send(parametros ,`numero=${numero}`);
 }
--- segundo parcial/introducir.php
+++ segundo parcial/introducir.php
@@ -3,12 +3,15 @@ if(isset($_POST['numero'])) {
 
   $numero = $_POST['numero'];
 
- 
+  echo '<form>';
 
   for($i=1; $i<=$numero; $i++) {
     echo "<input type='number'>";
+    echo"<br>";
+    echo"<br>";
+   
   }
 
-
+  echo '</form>';
 
 }
