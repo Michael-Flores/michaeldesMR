d89a2db86c9b588cad7c7547e71a4b69c48fb993
Michael-Flores <micha11.mrfv@gmail.com>
2023-12-01 23:20:50 -0400

    646sdhv

---
+++ parcial final/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <div class="inicio">
+       <div class=" inicio1">
+        <div id="sib-menu" class="me1">
+            <img src="imagen/usfx.png" alt="">
+            <button id="btn1">Opciónes</button>
+            <button id="btn2">Detalle 1</button>
+            <button id="btn3">Detalle 2</button>
+            <div id="sub-menu"></div>
+        </div>
+
+        <div class=" men2">
+            <div id="menu">
+                <button>Pregunta 1</button>
+                <hr >
+                <button>Pregunta 2</button>
+                <hr >
+                <button>Pregunta 3</button>
+                <hr >
+                <button>Pregunta 4</button>
+                <hr >
+                <button>Pregunta 5</button>
+            </div>
+            <div id="titulo"> Pregunta 1</div>
+            <div id="contenido">
+                <div class="contenido-fon">
+                    <div class="p1">
+                        <p style="font-weight: bold;">SIS 256 Programacion Web</p>
+                        <p>Examen Final - 02-12-2023 7:00 am</p>
+                    </div>
+                    <img src="imagen/yo.jpeg" alt="">
+                    <div class="p2">
+                     <p class="pa1">BARJA CORAGUA ERLINDA</p>
+                     <p>Carrera: Ingenieria de distemas</p>
+                     <p>Repositorio Examen: </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+       </div>
+
+
+       <div id="pie">
+            Sucre - Semestre 2-2023
+       </div> 
+    </div>
+</body>
+</html>
